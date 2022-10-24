2830be48af2ce9ec0af35f81caa6a81990432ff1
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-10-23 19:25:37 -0600

    migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :friends
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'home/about'
   # Defines the root path route ("/")
   # root "articles#index"
 end
