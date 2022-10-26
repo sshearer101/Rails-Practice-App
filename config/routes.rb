2ef8cd8f21b6648152e198b6d7b800db663b730e
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-10-25 20:00:42 -0600

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :friends
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+    resources :friends
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
