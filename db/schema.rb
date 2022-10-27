1b8546bb8adaa6b3be509b8fc3f4b0db922f800b
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-10-27 16:37:39 -0600

    association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_012459) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_221614) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_012459) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
